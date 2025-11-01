be0eed65d2f6195fb4de5b4fe5bfa387acee2ec9
sercansimsek <sercansimsek44@gmail.com>
2025-11-01 20:01:26 +0100

    images added according to different screen sizes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -25,16 +25,16 @@ export const Card = ({ data }) => {
   };
   return (
     <>
-      <div className="w-[327px] h-[319px] md:w-[213px] font-redHat">
+      <div className="w-[327px] h-[319px] md:w-[213px] lg:w-[250px] ont-redHat">
         <div className="flex flex-col justify-center items-center">
           <div className="relative">
             <picture>
               <source media="(min-width:1024px)" srcSet={data.image.desktop} />
               <source media="(min-width:768px)" srcSet={data.image.tablet} />
               <img
-                src={waffleImg}
+                src={image.mobile}
                 alt={data.name}
-                className={cn("h-[212px] md:h-[212px] rounded-md", {
+                className={cn("h-[212px] rounded-md", {
                   "border-2 border-red": isAdded,
                 })}
               />
@@ -55,13 +55,11 @@ export const Card = ({ data }) => {
 
             {isAdded && (
               <div
-                className={
-                  "absolute left-1/2 transform -translate-x-1/2 -bottom-[25px] z-10 flex justify-between items-center bg-red text-white border border-rose400 p-3 w-[160px] rounded-3xl"
-                }
+                className="absolute left-1/2 transform -translate-x-1/2 -bottom-[25px] z-10 flex justify-between items-center bg-red text-white border border-rose400 p-3 w-[160px] rounded-3xl"
               >
                 <button
                   type="button"
-                  className="flex items-center justify-center w-5 h-5 border-2 rounded-full cursor-pointer hover:bg-white"
+                  className="flex items-center justify-center w-5 h-5 border-2 rounded-full cursor-pointer hover:bg-white hover:bg-[url('src/assets/images/decrement-red.svg')]"
                   onClick={decrementCount}
                 >
                   <img src={decrementIcon} alt="decrement icon" />
@@ -71,7 +69,7 @@ export const Card = ({ data }) => {
                 </span>
                 <button
                   type="button"
-                  className="flex items-center justify-center w-5 h-5 border-2 rounded-full cursor-pointer hover:bg-white"
+                  className="flex items-center justify-center w-5 h-5 border-2 rounded-full cursor-pointer hover:bg-white hover:bg-[url('src/assets/images/increment-red.svg')]"
                   onClick={incrementCount}
                 >
                   <img src={incrementIcon} alt="increment icon" />
