4ab11c4d9afad67d9a04160397dee861d7709ec2
sercansimsek <sercansimsek44@gmail.com>
2025-11-03 11:34:13 +0100

    Card.jsx component implemented

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,3 @@
-import waffleImg from "../assets/images/image-waffle-mobile.jpg";
-import waffleImgTablet from "../assets/images/image-waffle-tablet.jpg";
 import cartImg from "../assets/images/icon-add-to-cart.svg";
 import incrementIcon from "../assets/images/icon-increment-quantity.svg";
 import decrementIcon from "../assets/images/icon-decrement-quantity.svg";
@@ -10,7 +8,7 @@ export const Card = ({ data }) => {
   const [isAdded, setIsAdded] = useState(false);
   const [productCount, setProductCount] = useState(1);
 
-  const { image } = data
+  const { image } = data;
   const incrementCount = () => {
     setProductCount((prev) => prev + 1);
   };
@@ -25,11 +23,11 @@ export const Card = ({ data }) => {
   };
   return (
     <>
-      <div className="flex flex-col gap-4 w-[327px] h-[319px] md:w-[213px] lg:w-[250px] font-redHat">
+      <div className="flex flex-col gap-4 w-[327px] md:w-[213px] lg:w-[250px] font-redHat">
         <div className="flex flex-col justify-center items-center mb-[25px]">
           <div className="relative">
             <picture>
-              <source media="(min-width:1024px)" srcSet={data.image.desktop}/>
+              <source media="(min-width:1024px)" srcSet={data.image.desktop} />
               <source media="(min-width:768px)" srcSet={data.image.tablet} />
               <img
                 src={image.mobile}
@@ -54,12 +52,10 @@ export const Card = ({ data }) => {
             )}
 
             {isAdded && (
-              <div
-                className="absolute left-1/2 transform -translate-x-1/2 -bottom-[25px] z-10 flex justify-between items-center bg-red text-white border border-rose400 p-3 w-[160px] rounded-3xl"
-              >
+              <div className="absolute left-1/2 transform -translate-x-1/2 -bottom-[25px] z-10 flex justify-between items-center bg-red text-white border border-rose400 p-3 w-[160px] rounded-3xl">
                 <button
                   type="button"
-                  className="flex items-center justify-center w-5 h-5 border-2 rounded-full cursor-pointer hover:bg-white hover:bg-[url('src/assets/images/decrement-red.svg')]"
+                  className="flex items-center justify-center w-5 h-5 border-2 rounded-full cursor-pointer hover:bg-white hover:bg-[url('./assets/images/decrement-red.svg')] hover:bg-[length:16px_16px] hover:bg-center hover:bg-no-repeat"
                   onClick={decrementCount}
                 >
                   <img src={decrementIcon} alt="decrement icon" />
@@ -69,10 +65,10 @@ export const Card = ({ data }) => {
                 </span>
                 <button
                   type="button"
-                  className="flex items-center justify-center w-5 h-5 border-2 rounded-full cursor-pointer hover:bg-white hover:bg-[url('src/assets/images/increment-red.svg')]"
+                  className="flex items-center justify-center w-5 h-5 border-2 rounded-full cursor-pointer hover-bg-white hover:bg-[url('./assets/images/increment-red.svg')] hover:bg-[length:16px_16px] hover:bg-center hover:bg-no-repeat"
                   onClick={incrementCount}
                 >
-                  <img src={incrementIcon} alt="increment icon" />
+                  <img src={incrementIcon} alt="increment icon"/>
                 </button>
               </div>
             )}
@@ -91,39 +87,6 @@ export const Card = ({ data }) => {
           )}`}</span>
         </div>
       </div>
-
-      {/*  <div className="w-[327px] h-[319px] font-redHat">
-        <div className="flex flex-col justify-center items-center ">
-          <img
-            src={waffleImg}
-            alt="waffle"
-            className="h-[212px] rounded-md border-2 border-red"
-          />
-          <div className="flex justify-between items-center bg-red text-[#fff] border border-rose400 p-3 w-[160px] rounded-3xl -m-[25px]">
-            <button
-              type="button"
-              className="flex items-center justify-center w-5 h-5 border-2 rounded-full cursor-pointer"
-            >
-              <img src={decrementIcon} alt="decrement icon" />
-            </button>
-            <span className="font-semibold text-sm">4</span>
-            <button
-              type="button"
-              className="flex items-center justify-center w-5 h-5 border-2 rounded-full cursor-pointer"
-            >
-              <img src={incrementIcon} alt="increment icon" />
-            </button>
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-2 mt-[41px]">
-          <span className="font-thin text-rose500 text-sm">Waffle</span>
-          <span className="font-semibold text-rose900 text-md">
-            Waffle with Berries
-          </span>
-          <span className="text-red text-md font-semibold">$6.50</span>
-        </div>
-      </div> */}
     </>
   );
 };
